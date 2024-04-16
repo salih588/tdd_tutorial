3281853954b982ea1d06e0f64c2e611a8f856f46
salihnsventures <muhammed.salih@nsventures.se>
2024-04-16 06:27:36 +0530

    handling error in create user completed

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,2 @@
+
+const kBaseUrl = 'https://65fd11b49fc4425c653114db.mockapi.io/test-api/';
--- lib/src/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/src/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:tdd_tutorial/core/errors/exceptions.dart';
+import 'package:tdd_tutorial/core/utils/constants.dart';
 import 'package:tdd_tutorial/src/authentication/data/models/user_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class AuthenticationRemoteDataSource {
   Future<void> createUser({
@@ -8,4 +13,46 @@ abstract class AuthenticationRemoteDataSource {
   });
 
   Future<List<UserModel>> getUsers();
+}
+
+const kCreateUserEndpoint = '/users';
+const kGetUsersEndpoint = '/users';
+
+class AuthRemoteDataSrcImpl implements AuthenticationRemoteDataSource {
+  final http.Client _client;
+
+  AuthRemoteDataSrcImpl(this._client);
+
+  @override
+  Future<void> createUser({required String createdAt,
+    required String name,
+    required String avatar}) async {
+    try {
+      final response = await _client.post(
+        Uri.parse('$kBaseUrl$kCreateUserEndpoint'),
+        body: jsonEncode({
+          'createdAt': createdAt,
+          'name': name,
+          'avatar': avatar,
+        }),
+      );
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw APIException(
+          message: response.body,
+          statusCode: response.statusCode,
+        );
+      }
+    }on APIException{
+      rethrow;
+    } catch(e){
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
+  }
+
+  @override
+  Future<List<UserModel>> getUsers() {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
 }
